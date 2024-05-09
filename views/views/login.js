61300c15fa00db3a372d3fc552e167608e60c41c
Saquib Hasnain <101635364+Saquib-Hasnain@users.noreply.github.com>
2024-05-10 01:37:35 +0530

    some

---
+++ views/views/login.js
@@ -0,0 +1,28 @@
+console.log("Script loaded");
+
+
+const login=async function login(e) {    
+    try {
+       e.preventDefault();
+
+
+        const loginDetails = {
+            email: e.target.email.value,
+            password: e.target.password.value
+        }
+        
+        console.log(loginDetails)
+        const response = await axios.post("http://localhost:3000/user/login", loginDetails)
+        alert(response.data.message)
+        localStorage.setItem('token', response.data.token)
+        window.location.href = "./expense.html"
+
+
+    } catch (err) {
+        document.body.innerHTML += `<div style="color:red;">${err}</div>`;
+
+    }
+
+}
+const form = document.querySelector('form');
+    form.addEventListener('submit', login);
